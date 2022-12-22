2403b3c5c1cbcac203303929497762ad78e384de
Cristian Pietro <crpietro26@gmail.com>
2022-12-22 15:17:22 -0300

    procurando candidato + voto nulo

---
+++ Projeto-urna/assets/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+              numero: '38111',
+              nome: 'Alphonse Eric',
+              partido: 'Metal',
+              fotos: [
+                {url: 'alphonse.jpg', legenda: 'Vereador'}
+              ]
+            },
+            {
+                numero: '77222',
+                nome: 'Edward Eric',
+                partido: 'Alquimista',
+                fotos:[
+                    {url: 'edward.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: ' Monkey D. Luffy',
+                partidos: 'One Piece',
+                vice: 'Roronoa Zoro',
+                fotos: [
+                    {url: 'luffy.jfif', legenda: 'Prefeito'},
+                    {url: 'zoroo.jfif', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Naruto Uzumaki',
+                partido: 'konoha',
+                vice: 'Sasuke Uchiha',
+                fotos: [
+                {url: 'naruto.png', legenda: 'Prefeito'},
+                {url: 'sasuke.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
